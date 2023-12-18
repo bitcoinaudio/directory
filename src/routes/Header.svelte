32c8d75ab25c47262a9e87ec096581f77b6ccaf8
radWorx <dramirez@soulrivers.com>
2023-12-17 22:52:22 -0500

    Update Header.svelte

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { page } from '$app/stores';
 	import logo from '$lib/images/ba-logo.svg';
+	import github from '$lib/images/github.svg';
 </script>
 
 <header>
@@ -15,15 +16,13 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<!-- <li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
-			</li> -->
+			</li>
 			<!-- <li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li> -->
-			<!-- <li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li> -->
+			
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
@@ -31,9 +30,9 @@
 	</nav>
 
 	<div class="corner">
-		<!-- <a href="https://github.com/sveltejs/kit">
+		<a href="https://github.com/bitcoinaudio/directory">
 			<img src={github} alt="GitHub" />
-		</a> -->
+		</a>
 	</div>
 </header>
 
