dbc6f45054a9dccc39e291bf3507ecf5e4ffbe41
radWorx <dramirez@soulrivers.com>
2024-01-11 06:44:27 -0500

    Update +page.svelte

--- src/routes/rules/+page.svelte
+++ src/routes/rules/+page.svelte
@@ -3,20 +3,102 @@
 	<meta name="description" content="Submit here" />
 </svelte:head>
 
-<div class="text-column">
+<div class="rules">
+	<h1><b>ATTENTION BTC AUDIO PEEPS</b></h1>
 	<h1>These are the rules</h1>
-
 	<p>
-		
+		First things first, we copied most of these rules from Fake Rare. 
+		Because I have it on good authority the rules were CC0. 
+		DON’T WORRY… I punched them up a little bit and made them relevant to your thing. 
 	</p>
-
 	
-
 	<p>
-		
+		You need a token… to the <a href="https://xchain.io/asset/NORMIES" >normies</a>, this can be a little intimidating, so please don’t hesitate to ask your fellow… not even sure what we call yall yet. 
+		Something like the Beyhive but for music on bitcoin. 
+		Audiac sounds like a person who is sexually attracted to Audis, and I wouldn’t put that evil on you.
 	</p>
 
 	<p>
-		
+		Anyway… you can always get help in the Bitcoin Audio telegram channel. 
+		If determined to teach yourself, click <a href="http://robotlovecoffee.io/CreatorFW.html">here</a> to learn how to create an asset. 
+		You’ll need 0.5 XCP (and a little BTC for the miner fee… and the perfect name… it says “between 4 and 14 characters” but this is fake news… it can’t be more than 12). 
+		Simple so far right?
 	</p>
+
+	<p>
+		To publish in the Bitcoin Audio Directory, you will need to sacrifice a <a href="https://xchain.io/asset/fakeasf">BTCAUDIO</a>, only after your card is approved. 
+		They’re fancy… and rich in history. If you need one, feel free to ping the group to inquire about trades, or pick one up from a dispenser. 
+		Please do not destroy anything until your card has been approved… NO REFUNDS. 
+		Also the token’s are not controlled by us. Once we dispersed them (first 100 people in the telegram chat) the market will do what it does. We don’t diddle in the markets doings. 
+	</p>
+
+	<p>
+		Once approved, your card will appear on <a href="directory.bitcoinaudio.co">Bitcoin Audio Directory</a>… and <a href="https://pepe.wtf/new/collections/Fake-Rares">pepe.wtf</a>… and probably all over twitter. 
+		Don’t feel compelled to DM the team to check on the status. 
+		Persistence is key in many things… but here it will only distract and delay. 
+		Also it makes us think you are all frothing in the loins for us and it will give us a big head. 
+		Then we’ll be wearing sunglasses at nighttime and REALLY not returning ur call, cause we’re out being fuck boi’s and wearing mad curve cologne at the rave we’re all to old for. 
+		Do you want that? Blow up our phone if you want. We’ll answer that shit on video chat wearing our stunner shades while having fancy brunch and you are getting hung up on.
+	</p>
+	<b>Audio Rules:</b>
+	<ol>
+		<li><b>Cards must be 1200×1680.</b>That is sufficiently high enough res to where we can see all the subtle intricacies that make you the brilliant little koala your mom and public school always said you were. 
+			Please don’t send us whatever google earth sized image you think is artistically necessary. We check these messages on a Nokia 3310, and if your enhanced pixels break our corporate property there will be hell to pay. 
+			Don’t doubt this. We got lawyers out the ying yang and are super ready to Judge Joe Brown your shit. You can link a high-resolution video to your asset, but make sure it follows the same aspect ratio please and thanks.
+		</li>
+		<li>
+			<b>Your card can look like a trading card, but this is not required.</b> It can be animated/audio gif or MP4 as well. 
+			It doesn’t have to be a frog… but we like frogs. It doesn’t have to be Bitcoin Church Art. But we like Bitcoin Church Art. 
+			Just be you. It DOES have to have music/audio. Some series will have a theme, stick to it. Don’t be the guy who shows up to the costume party dressed as himself. 
+			That guy usually wears rayon boot cut dress pants. Don’t be that guy.
+		</li>
+		<li><b>Issuance must be LOCKED </b>so your card cannot be inflated. You are not Janet Yellen just cranking out more whenever you are short at the grocery store. Pick a number and know its final. This may be new technology so our advice is patience. Move slowly. 
+			Think before you ink. Don’t be a person stuck shitting in the bathtub because you cant figure out how to flush the toilet.</li>
+		<li><b>Your card must NOT be DIVISIBLE…</b>when creating an asset, the default is set to divisible, so make sure you click the toggle, otherwise you’ll have to pick a new name for your card. This is not OF. 
+			You cant just be selling fractions of yourself. We may make a collection for that someday. But this aint it.</li>
+		<li><b>No NSFW content please…</b> we don’t have HR on staff and we’re all adults here… so use your best judgement. 
+			As the person editing these rules I will tell you that penis’ make me sad. All of them. Even in paintings. I only like mine cause I know it came from a good family. 
+			If you send in your rhubarb, I'm going to need a vacation and then NOBODYS getting approved.</li>
+		<li><b>You must issue at least 21 tokens</b>and no more than 2,100. I don’t know why. Our number is actually less than Fake Rare. I know… I just lowered it. Like a fucking task master. Imagine my audacity. 
+			I did this arbitrarily too but I’ll probably spin up some revisionist history bullshit as to why I did it and it was for the best. </li>
+		<li><b>IF YOU PURPOSEFULLY DESTROY YOUR CARD BELOW THE 21 THEN YOU WILL BE SUBJECTED TO LOSE YOUR SLOT – EVEN IF APPROVED – THIS IS KNOWN AS THE DANKFARMS ACT OF 2023.</b></li>
+		<li>Please be respectful of the rules, sometimes the telegram chat is the best place to ask questions, if you don’t get clarity here- please always visit <a href="https://t.me/FakeLaboratory">https://t.me/FakeLaboratory</a> when in doubt. 
+			That is not a typo. I'm literally sending you to another collections help desk. That is the kind of 8D Chess we play here at Bitcoin Audio. They may not want to help you. Make them. Tell them you’ll be there best friend. Offer those dick pic’s we didn’t want. Get creative. 
+			Point is… they are nice people and will probably help you. That’s what we’re betting on. Real socialism. </li>
+		<li>No websites and no QR codes please. We are still pretending to make art here… its not a NASCAR. 
+			Class it up. Send them to your Etsy with an airdrop or something if you want to sell them scented candles or your shitty pottery.</li>
+		<li>The general rule of thumb is only one submission per artist until approved or denied. After approval or denial you may submit again. 
+			If you’ve been waiting a while and have a fire new piece… you may wanna swap for your previous submission. If your submission is good but we think it will fit an upcoming series we will offer it. Its not a double offer. 
+			I promise. Some artists will show up more than once in a series. No one cuts the line… they applied.
+		</li>
+		<li>We don’t get paid to do this… we have real life responsibilities and we move as fast as we can, but the best shit takes time. Please bear with us as we sort through your submissions. 
+			Also its not like “here look at my card” the Scientists are listening to all that magic special stuff that nobody actually cares about. That snare drum you spent like 4hrs mixing to get “just right”. 
+			Yeah…. they heard that shit. Got all frothed up over it chances are. So don’t go ruining the sexual magic you have generated by storming in, and smacking the headphones off of them and asking why they haven’t sent a limo to your house yet cause its OBVIOUS to anyone with ears that you are destined for the BIG TYME. We’ve got an assortment of chill pills.</li>
+		<li>Don’t share or sell your card prior to approval, otherwise we’ll have to kick your submission… once published, please feel free to drop your piece however you see fit… pause. 
+			People often forget this rule. Don’t be the person who is doing the puppy dog eyes because you sent it to fast freddy and he sent it to Joey Marinara and he thought it would be a super dope flexington to put that joint on twitter, or Musk Talk Radio, or whatever they call it in the future. 
+			Don’t get your card kicked because you cant keep a secret, being a Leaky Larry. We love Scarce.City and the PPS, but to each his own.</li>
+		<li>Remember, this is all music. We are supposed to be cool, and have fun. I know you want to make a big difference with the spotted whales or the homeless turtles but this is about fun. Its crypto. 
+			Nobody has a tie on, and the ones that do probably have cartoons and ketchup on em. We will take every submission as seriously as we’d want our own taken because we are artists too. Some people are new, some have been in music forever. 
+			Try to make it a nice neighborhood. You are not Simon Cowell. So please (for me) don’t go turning the chat into your personal version of Stereo Gum with big ass think pieces about how someone’s trombone “languished for the contemporary”. 
+			Or whatever ideas are clinking around in your nugget. We are also not going to pour pancake syrup in each others mouth and RDHT. So lets find a middle ground together.</li>
+	</ol>
+	<h2><b>NOTE: Please don’t destroy your BTCAUDIO until asked to do so</b>… once your submission is accepted, someone will give you further instructions on the sacrifice. You can destroy the token by using the Freewallet.io desktop application.
+
+		If you need us, find us on telegram.
+		</h2>
 </div>
+<style>
+	.rules {
+		
+		color: black;
+		background-color: rgb(250, 250, 250);
+		border: 2px solid black;
+		border-radius: 1%;
+		box-shadow: -5px -5px 10px 7px #060606a0;
+		padding: 10px;
+	}
+	a {
+		color: chocolate; 
+	}
+</style>
+
