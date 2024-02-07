dff4aafc6535d72eedd0d3b131958e86d78afa39
radWorx <dramirez@soulrivers.com>
2024-02-06 22:10:10 -0500

    Update Connect.svelte

--- src/components/Connect.svelte
+++ src/components/Connect.svelte
@@ -123,58 +123,10 @@
 		}
 	}
 
-	async function dropdownFiller() {
-		//GetMyBitmaps();
-	}
-
-	function handleSubmit(html) {
-		console.log('html', html);
-	}
-
-	let selected;
-	let selectedInscription;
-	let selectedmimetype;
-	let cpAddress = ''
-	let message = 'Bitcoin%20Audio%20is%20RAD!'
-	let cpURL =	"counterparty:"+ cpAddress + "?action=sign&message="+ message + "&icon=&callback=http%3A%2F%2Flocalhost:3000%2Fapi%2FverifyMessage"
-
-	async function isCPVerified() {
-		let cpURL =	"counterparty:"+ cpAddress + "?action=sign&message="+ message + "&icon=&callback=http%3A%2F%2Flocalhost:3000%2Fapi%2FverifyMessage"
-
-		console.log(cpAddress)
-		console.log(cpURL)
-	// var file = "<file contents here>";
-	var requestOptions = {
-  	method: 'POST',
-  	
-  	redirect: 'follow'
-};
-
-// fetch("http://localhost:3000/api/verifyMessage?address=" + {cpAddress} + "&message=" + {message} + "&signature=H4ojQ%2BmaI7ikDSs7rrcdgFzogXT4P9Lue6MrtYDqZAdLAEPl9twBj%2BPhUX9IfUgw36GvRdLv44cP3n35LAL9Wxg%3D", requestOptions)
-// fetch(cpURL, requestOptions)
-
-// .then(response => response.text())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
-		//
-	}
-
-
-// fetch("http://localhost:3000/api/verifyMessage?address=" + {cpAddress} + "&message=" + {message} + "&signature=IPPTJW08XhfGoTRYYcXz%2FCrx5Ymjq5HKiMS0dKpBsoR7FH%2B8Sa0JoKYxgeQ6wTdHuDd7xErIz2GqOgerU9Mkp4Y%3D", requestOptions)
-//   .then(response => response.text())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
-// 		//
-// 	}
-
-	// counterparty:{cpAddress} + ?action=sign&message=+ {message} +&icon= + {icon} + &callback=http%3A%2F%2Flocalhost:3000%2Fapi%2FverifyMessage
 	
 </script>
 
 <div class="wallet">
-	<!-- <button on:click={isCPVerified}><img class="wallet-logo" src={freewallet} alt="" /></button> -->
-	<!-- <a href={cpURL}><img class="wallet-logo" src={freewallet} alt="" /></a>
-	<input bind:value={cpAddress} /> -->
 </div>
 <hr />
 <div class="wallet">
@@ -188,30 +140,6 @@
 		>
 	{/if}
 </div>
-
-
-<!-- <select class="dropdown" bind:value={selectedInscription}  on:change={() => handleSubmit(selectedInscription)}>
-	<option>My Inscriptions</option>
-	{#await dropdownFiller()}
-	<p>Loading Inscriptions</p>
-	{:then htmls}
-			{#each $htmlarray as html, index}
-			<option value={index}>{html}</option>
-
-			{/each}
-	{/await}
-</select>
-<select class="dropdown" bind:value={selectedmimetype}  on:change={() => handleSubmit(selectedmimetype)}>
-	<option>mimetypes</option>
-	{#await dropdownFiller()}
-	<p>Loading mimetypes</p>
-	{:then htmls}
-			{#each mimetypes as mime, index}
-			<option value={index}>{mime}</option>
-
-			{/each}
-	{/await}
-</select> -->
 
 <style>
 	.wallet {
