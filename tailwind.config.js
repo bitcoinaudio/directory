1a0e1dbe9392d158b5840b9dd4471d92691403f7
bitcoinaudio <bitcoinaudio@gmail.com>
2024-12-03 23:43:31 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
@@ -12,7 +13,7 @@ export default {
     }
   },
   plugins: [
-    require('daisyui'),
+    daisyui,
   ],
   daisyui: {
     themes: [ 
