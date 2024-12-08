1187064126ad114a8b09e9e487d4a195f8913416
bitcoinaudio <bitcoinaudio@gmail.com>
2024-12-08 01:55:15 -0500

    Update Collections.svelte

--- src/components/Collections.svelte
+++ src/components/Collections.svelte
@@ -1,6 +1,7 @@
 <script>
  import collections from '../collections';
  import ordImage from '$lib/images/ordinals.svg';
+ 
 
 </script>
  
@@ -54,7 +55,7 @@
                 </a>
               </li>
               <li>
-                <a class="tooltip" data-tip="Details" href={"/" + item.insID} target="_blank">
+                <a class="tooltip" data-tip="Details" href={"https://ordinals.com/content" + item.insID} target="_blank">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     class="h-5 w-5"
