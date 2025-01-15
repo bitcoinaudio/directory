9a731e27932e021d022fe6b4464dd64bb67e1394
bitcoinaudio <bitcoinaudio@gmail.com>
2025-01-14 19:53:45 -0500

    Update Collections.svelte

--- src/components/Collections.svelte
+++ src/components/Collections.svelte
@@ -43,7 +43,7 @@
                 </a>
               </li>
               <li>
-                <a class="tooltip" data-tip="Details" href={"https://ordinals.com/content" + item.insID} target="_blank">
+                <a class="tooltip" data-tip="Details" href={"https://ordinals.com/content/" + item.insID} target="_blank">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     class="h-5 w-5"
