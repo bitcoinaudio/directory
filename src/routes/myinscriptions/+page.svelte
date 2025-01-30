18c9df251cd09935dc4750095b1ed07312324dd7
bitcoinaudio <bitcoinaudio@gmail.com>
2025-01-30 14:21:05 -0500

    Update +page.svelte

--- src/routes/myinscriptions/+page.svelte
+++ src/routes/myinscriptions/+page.svelte
@@ -153,7 +153,7 @@
 								</li>
 							{:else}	
 								<li>
-									<a class="tooltip" href="/" data-tip="We are very sorry, but it appears your wallet does not possess the required hydrationing to acess the raw power of The Ides Of March. Please return when you have acquired the fortitude and vitality for this ordinal experience">NON OWNER</a>
+									<a class="tooltip" href="/" data-tip="We are very sorry, but it appears your wallet does not possess the required hydrationing to acess the raw power of Inscribed Audio. Please return when you have acquired the fortitude and vitality for this ordinal experience">NON OWNER</a>
 								</li>
 							{/if}
 						</ul>
